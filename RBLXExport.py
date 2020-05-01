c891da8271f08f3f1408044e5b13280ce13d66f6
thoricelli <roanheylen@hotmail.com>
2020-05-01 19:23:52 +0200

    Update - Display information

--- RBLXExport.py
+++ RBLXExport.py
@@ -3,6 +3,8 @@ import os
 import re
 import getpass
 
+print("Please wait...")
+
 path = "C:\\Users\\" + getpass.getuser() + "\\AppData\\Local\\Temp\\Roblox"
 path2 = os.getcwd()
 ExportPath = path2 + "\\Export\\"
@@ -20,6 +22,7 @@ for i in arr:
 		for lines in f:
 			curlines = curlines+1
 			if lines.find("PNG",1,4) == 1:
+				print("Found image")
 				c = open(ExportPath + i + '.png', "wb+")
 				count = 0
 				f.close()
@@ -31,7 +34,7 @@ for i in arr:
 				c.close()
 				break
 			if lines == "Content-Type: audio/mpeg\n": #For some reason find wouldn't work...
-				print("Found")
+				print("Found audio")
 				c = open(ExportPath + i + '.mp3', "wb+")
 				count = 0
 				f.close()
@@ -43,3 +46,5 @@ for i in arr:
 				c.close()
 				break
 		f.close()
+print("Done!")
+raw_input("Press Enter to continue...")
