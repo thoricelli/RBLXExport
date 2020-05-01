85f48a4aba3c0bf252cfde68699baf76324b5226
thoricelli <roanheylen@hotmail.com>
2020-05-01 19:05:35 +0200

    Add files via upload

---
+++ RBLXExport.py
@@ -0,0 +1,45 @@
+#Made by thoricelli 2020, ignore my terrible programming lol
+import os
+import re
+import getpass
+
+path = "C:\\Users\\" + getpass.getuser() + "\\AppData\\Local\\Temp\\Roblox"
+path2 = os.getcwd()
+ExportPath = path2 + "\\Export\\"
+os.chdir(path)
+
+if not os.path.isdir(ExportPath):
+	os.mkdir(ExportPath)
+
+arr = os.listdir("http/")
+for i in arr:
+	if not os.path.isfile(ExportPath + i):
+		f = open("http/" + i,"r")
+		lines = 0
+		curlines = 0
+		for lines in f:
+			curlines = curlines+1
+			if lines.find("PNG",1,4) == 1:
+				c = open(ExportPath + i + '.png', "wb+")
+				count = 0
+				f.close()
+				f = open("http/" + i,"rb")
+				for x in f:
+					count = count + 1
+					if count > curlines-1:
+						c.write(x)
+				c.close()
+				break
+			if lines == "Content-Type: audio/mpeg\n": #For some reason find wouldn't work...
+				print("Found")
+				c = open(ExportPath + i + '.mp3', "wb+")
+				count = 0
+				f.close()
+				f = open("http/" + i,"rb")
+				for x in f:
+					count = count + 1
+					if count > curlines+12:
+						c.write(x)
+				c.close()
+				break
+		f.close()
